82c6c1e4fe4a7883e0b8fe742c767452185a05c6
Andrey Adamovich <andrey@aestasit.com>
2018-11-01 10:10:03 +0200

    intial commit

---
+++ src/com/playtech/Main.java
@@ -0,0 +1,9 @@
+package com.playtech;
+
+public class Main {
+
+    public static void main(String[] args) {
+      System.out.println("Hello, World!");
+      System.out.println("Yeah!");
+    }
+}
